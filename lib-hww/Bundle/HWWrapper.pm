68faca8b7f5800b262f375bb37fb89f5c58467d1
tyru <tyru.exe@gmail.com>
2009-08-08 16:56:36 +0900

    add Bundle::HWWrapper
    to install prereq modules,
    run 'perl -MCPAN -e "install Bundle::HWWrapper"'
    or 'cpan -i Bundle::HWWrapper'.

---
+++ lib-hww/Bundle/HWWrapper.pm
@@ -0,0 +1,40 @@
+package Bundle::HWWrapper;
+
+our $VERSION = '0.0.0';
+
+
+1;
+__END__
+
+=head1 NAME
+
+Bundle::HWWrapper - Bundling Modules of HWWrapper
+
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::HWWrapper'
+
+
+=head1 CONTENTS
+
+# for HW
+LWP::UserAgent
+HTTP::Request::Common
+Crypt::SSLeay
+Class::Accessor::Lvalue
+URI
+
+# for HWWrapper
+Date::Manip
+DateTime
+HTML::Template
+HTML::TreeBuilder
+Text::Hatena
+XML::TreePP
+LWP::Authen::Wsse
+
+
+=head1 AUTHOR
+
+tyru <tyru.exe@gmail.com>
