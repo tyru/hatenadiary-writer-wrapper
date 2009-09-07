54b3b709a39da2a2c710ad8cbf7baf1379358abc
tyru <tyru.exe@gmail.com>
2009-09-07 19:21:23 +0900

    change what 'init' command does
    'init' copies 'config-hww.txt.sample' to 'config-hww.txt'.
    and delete 'cookie.txt' if it exists.

--- lib-hww/HWWrapper/Commands/Init.pm
+++ lib-hww/HWWrapper/Commands/Init.pm
@@ -14,14 +14,6 @@ sub regist_command {
     $HWW_COMMAND{init} = {
         coderef => \&run,
         desc => 'set up hww environment',
-        option => {
-            'c|config' => {
-                desc => "apply config's settings",
-            },
-            'd|delete' => {
-                desc => "delete cookie file",
-            },
-        },
     };
 }
 
@@ -29,86 +21,30 @@ sub regist_command {
 sub run {
     my ($self, $args, $opt) = @_;
 
-    my $opt_config = $opt->{'c|config'};
-    my $opt_delete = $opt->{'d|delete'};
-
-    my $txt_dir = 'text';
-    my $config_file = 'config.txt';
-    my $hww_config_file = 'config-hww.txt';
     my $cookie_file = 'cookie.txt';
+    my $config_file = 'config-hww.txt';
+    my $config_file_sample = 'config-hww.txt.sample';
 
-    my $dir = shift @$args;
-    if (defined $dir) {
-        $txt_dir = $dir;
-    }
-    elsif ($opt_config) {
-        $txt_dir = $self->txt_dir;
-        $config_file = $self->config_file;
-        $hww_config_file = $self->config_hww_file;
-        $cookie_file = $self->cookie_file;
-    }
-
-    my $config_data = <<EOT;
-id:yourid
-txt_dir:$txt_dir
-touch:@{[ File::Spec->catfile($txt_dir, 'touch.txt') ]}
-client_encoding:utf-8
-server_encoding:euc-jp
-EOT
-
-    if ($opt_delete && -f $cookie_file) {
+    # delete $cookie_file.
+    if (-f $cookie_file) {
         unlink $cookie_file
-            or $self->warning("$cookie_file: $!")
+            or $self->warning("$cookie_file: $!");
     }
 
-
-    # text dir
-    if (-d $txt_dir) {
-        puts("directory $txt_dir already exists.");
-    }
-    else {
-        mkdir $txt_dir or $self->warning("$txt_dir: $!");
-        puts("mkdir $txt_dir.");
-    }
-
-    # hw config file
-    my $made_config_file;
-    if (-f $config_file) {
-        puts("file $config_file already exists.");
-    }
-    else {
-        my $FH = FileHandle->new($config_file, 'w');
-        if (defined $FH) {
-            $FH->print($config_data);
-            $FH->close;
+    # copy $config_file_sample to $config_file.
+    unless (-f $config_file) {
+        unless (-f $config_file_sample) {
+            $self->warning(
+                "$config_file_sample was not found." .
+                "skip make $config_file..."
+            );
         }
         else {
-            $self->warning("$config_file: $!");
+            $self->require_modules(qw(File::Copy));
+            File::Copy::copy($config_file_sample => $config_file)
+                or
+            $self->warning("Cannot copy $config_file_sample to $config_file: $!");
         }
-
-        puts("create $config_file.");
-        $made_config_file = 1;
-    }
-    puts("chmod 0600 $config_file");
-    chmod 0600, $config_file or $self->warning($!);
-
-    # hww config file
-    if (-f $hww_config_file) {
-        # make this private
-        # because it may contain username and password.
-        puts("chmod 0600 $hww_config_file");
-        chmod 0600, $hww_config_file or $self->warning($!);
-    }
-
-    # cookie file
-    if (-f $cookie_file) {
-        puts("chmod 0600 $cookie_file");
-        chmod 0600, $cookie_file or $self->warning($!);
-    }
-
-
-    if ($made_config_file) {
-        puts("\nplease edit your id in $config_file.");
     }
 }
 
